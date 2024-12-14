b6a17d553f318c8c91b59cf26f2c3ee1b40247ae
Nords Zamora <100557534+nordszamora@users.noreply.github.com>
2024-12-13 23:52:34 -1200

    Update User.jsx version 1.1 fixed

--- frontend/src/components/protected/User.jsx
+++ frontend/src/components/protected/User.jsx
@@ -15,51 +15,52 @@ import { AUTH_SUCCESS, USER_DATA, LOGOUT } from '../api/user/api';
 import './style.css';
 
 export default function User() {
-   const [copySuccess, setCopySuccess] = useState('');
+  const [copySuccess, setCopySuccess] = useState('');
 
-   // Response data from the AUTH_SUCCESS & USER_DATA
-   const { data: IsAuth, error: IsAuthIssue, isLoading: IsAuthLoading} = AUTH_SUCCESS()
-   const { data: UserData, error: UserIssue, isLoading: UserLoading} = USER_DATA()
-  
-   // Mutation function to logout account
-   const { mutate } = LOGOUT(IsAuth?.csrf_token)
+  // Response data from the AUTH_SUCCESS & USER_DATA
+  const { data: IsAuth, error: IsAuthIssue, isLoading: IsAuthLoading } = AUTH_SUCCESS();
+  const { data: UserData, error: UserIssue, isLoading: UserLoading } = USER_DATA();
 
-   // Check loading states and return null or navigate away as needed
-   if(IsAuthLoading && UserLoading) return null;
-   if(IsAuthIssue && UserIssue) return <Navigate to='/signin/'/>;
+  // Mutation function to logout account
+  const { mutate } = LOGOUT(IsAuth && IsAuth?.csrf_token);
 
-   // Form submission to logout account
-   function logout() {
-     // Make an API request to logout account
-     mutate()
-   }
+  // Check loading states and return null or navigate away as needed
+  if (IsAuthLoading && UserLoading) return null;
+  if (IsAuthIssue && UserIssue) return <Navigate to='/signin/' />;
 
-   // Handle copying the link to the clipboard
-   const handleCopy = () => {
-     setCopySuccess('Link copied!');
-     setTimeout(() => setCopySuccess(''), 3000);
-   };
+  // Form submission to logout account
+  function logout() {
+    mutate(); // Make an API request to logout account
+  }
 
-   return (
-     <>
-      {
-        IsAuth?.isauth && (
-          <div>
+  // Handle copying the link to the clipboard
+  const handleCopy = () => {
+    setCopySuccess('Link copied!');
+    setTimeout(() => setCopySuccess(''), 3000);
+  };
+
+  return (
+    <>
+      {IsAuth && IsAuth?.isauth ? (
           <Container>
             <Row>
               <h1 className="banner mt-4">StealthMessage</h1>
               <Col className="d-flex align-items-center justify-content-center">
                 <nav className="p-2 mt-4">
-                  <Button variant="danger" as={Link} to={`/user/notification`}>
-                     Message <Badge bg="success">{(UserData?.secret_messages.filter(data => data.has_read === 0).length)}</Badge>
+                  <Button
+                    variant="danger"
+                    as={Link}
+                    to={`/user/notification`}
+                  >
+                    Message <Badge bg="success">{UserData && UserData?.secret_messages.filter(data => data.has_read === 0).length}</Badge>
                   </Button>
                 </nav>
 
                 <nav className="p-2 mt-4">
-                  <DropdownButton id="dropdown-basic-button" title={`${UserData?.user_data.name}`} variant="secondary">
-                      <Dropdown.Item as={Link} to={`/`}>Home</Dropdown.Item>
-                      <Dropdown.Item as={Link} to={`/user/settings`}>Settings</Dropdown.Item>
-                      <Dropdown.Item as={Link} to={`/signin`} onClick={logout}>Logout</Dropdown.Item>
+                  <DropdownButton id="dropdown-basic-button" title={`${UserData && UserData?.user_data.name}`} variant="secondary">
+                    <Dropdown.Item as={Link} to={`/`}>Home</Dropdown.Item>
+                    <Dropdown.Item as={Link} to={`/user/settings`}>Settings</Dropdown.Item>
+                    <Dropdown.Item as={Link} to={`/signin`} onClick={logout}>Logout</Dropdown.Item>
                   </DropdownButton>
                 </nav>
               </Col>
@@ -67,28 +68,29 @@ export default function User() {
 
             <Row className="d-flex align-items-center justify-content-center mt-4">
               <Col md={5}>
-               <div className='p-4'>
-                 <p style={{color: 'white', textAlign: 'center', fontFamily: 'monospace'}}>Send this to everyone</p>
-                 <Form>
-                   <Form.Group controlId="exampleForm.ControlInput1" className="d-flex align-items-center">
-                     <Form.Control 
-                       type="text" 
-                       value={`${location.protocol}://${location.hostname}:${location.port}/secret/${UserData?.user_data?.message_key}`} // Remove the "${location.port}" in production
-                       readOnly
-                     />
-                     <CopyToClipboard text={`${location.protocol}://${location.hostname}:${location.port}/secret/${UserData?.user_data?.message_key}`} onCopy={handleCopy}> {/* Remove the "${location.port}" in production */}
-                       <Button variant="light" className="ml-2">Copy</Button>
-                     </CopyToClipboard>
-                   </Form.Group>
-                   {copySuccess && <p style={{ color: 'lightgreen', textAlign: 'center', marginTop: '10px' }}>{copySuccess}</p>}
-                 </Form>
-               </div>
+                <div className='p-4'>
+                  <p style={{ color: 'white', textAlign: 'center', fontFamily: 'monospace' }}>Send this to everyone</p>
+                  <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1" className="d-flex align-items-center">
+                      <Form.Control
+                        type="text"
+                        value={`${location.protocol}//${location.hostname}/secret/${UserData && UserData?.user_data?.message_key}`} // Remove the "${location.port}" in production
+                        readOnly
+                      />
+                      <CopyToClipboard
+                        text={`${location.protocol}//${location.hostname}/secret/${UserData && UserData?.user_data?.message_key}`}
+                        onCopy={handleCopy} // Remove the "${location.port}" in production
+                      >
+                        <Button variant="light" className="ml-2">Copy</Button>
+                      </CopyToClipboard>
+                    </Form.Group>
+                    {copySuccess && <p style={{ color: 'lightgreen', textAlign: 'center', marginTop: '10px' }}>{copySuccess}</p>}
+                  </Form>
+                </div>
               </Col>
             </Row>
           </Container>
-        </div>
-        )
-      }
-     </>
-   );
+      ) : null}
+    </>
+  );
 }
